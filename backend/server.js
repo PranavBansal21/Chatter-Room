d6663fb21bdd3a8a81f74cb44e48ae49016af7bd
Pranav <pranav123bansal@gmail.com>
2024-07-04 14:27:57 +0530

    dnd

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-//hEY
+//hEyy
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
